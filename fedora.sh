73e4a53a1eacec95721a18b2dfea152ed240b2f5
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2025-01-26 21:01:12 -0500

    use hypridle instead of swayidle
    hypridle supports dbus inhibit

--- fedora.sh
+++ fedora.sh
@@ -12,9 +12,9 @@ dnf update -y @multimedia
 
 dnf swap -y sway-config-fedora sway-config-upstream
 dnf remove -y abrt vim-minimal sddm Thunar xarchiver nano parted pinfo rsync tracker wget2 \
-	grimshot
+	grimshot swayidle
 dnf --setopt=install_weak_deps=False install -y neovim
 dnf install -y power-profiles-daemon jetbrains-mono-nl-fonts ripgrep brightnessctl wmenu \
-	zathura zathura-pdf-mupdf clipman mutt isync lynx calcurse udiskie
+	zathura zathura-pdf-mupdf clipman mutt isync lynx calcurse udiskie hypridle
 
 ln -s /usr/bin/nvim /usr/bin/vi
