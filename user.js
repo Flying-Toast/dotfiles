7bbc9e3fcb43aeb52ce839ebe24c28a771843239
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2025-07-12 11:07:19 -0700

    firefox: always show the full url

--- user.js
+++ user.js
@@ -1,6 +1,8 @@
 user_pref("browser.download.start_downloads_in_tmp_dir", true);
 user_pref("browser.fullscreen.autohide", false);
 user_pref("browser.tabs.insertAfterCurrent", true);
+user_pref("browser.urlbar.showSearchTerms.enabled", false);
+user_pref("browser.urlbar.trimURLs", false);
 user_pref("extensions.pocket.enabled", false);
 user_pref("network.IDN_show_punycode", true);
 user_pref("reader.parse-on-load.enabled", false);
